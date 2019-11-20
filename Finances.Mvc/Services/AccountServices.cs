1c472effc8ccbc634dc6fffbcf99ecd252fac3dc
Kate Kaidashova <kate.kaidashova@endobit.de>
2019-11-20 23:47:57 +0100

    add dapper for queries

---
+++ Finances.Mvc/Services/AccountServices.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using Finances.Mvc.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Finances.Mvc.Services
+{
+    public class AccountServices
+    {
+        private readonly IDbConnection db;
+
+        public AccountServices(ApplicationDbContext db)
+        {
+            this.db = db.Database.GetDbConnection();
+        }
+
+        public string[] GetCategories()
+        {
+            db.QueryAsync<string[]>("select name from AccountItems");
+        }
+
+
+    }
+}
